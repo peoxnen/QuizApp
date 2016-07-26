a1bd8257d1c0c6a53d356611bacb238cc6b667c9
peoxnen <sienski.witold@gmail.com>
2016-07-26 20:57:51 +0200

    stetho and timber for debug

--- app/src/main/java/iview/wsienski/quizapp/QuizApp.java
+++ app/src/main/java/iview/wsienski/quizapp/QuizApp.java
@@ -2,10 +2,13 @@ package iview.wsienski.quizapp;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
 import iview.wsienski.quizapp.di.component.AppComponent;
 import iview.wsienski.quizapp.di.component.DaggerAppComponent;
 import iview.wsienski.quizapp.di.module.AppModule;
 import iview.wsienski.quizapp.di.module.NetModule;
+import timber.log.Timber;
 
 /**
  * Created by Witold Sienski on 26.07.2016.
@@ -19,6 +22,11 @@ public class QuizApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this);
+            Timber.plant(new Timber.DebugTree());
+        }
 
         appComponent = DaggerAppComponent.builder()
                 .appModule(new AppModule(this))
