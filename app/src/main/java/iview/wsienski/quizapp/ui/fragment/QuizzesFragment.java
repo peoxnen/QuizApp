e9739a2f5523a7fbba5e783d6eebae17d631c14a
peoxnen <sienski.witold@gmail.com>
2016-07-26 21:07:22 +0200

    material progressbar and dialogs

--- app/src/main/java/iview/wsienski/quizapp/ui/fragment/QuizzesFragment.java
+++ app/src/main/java/iview/wsienski/quizapp/ui/fragment/QuizzesFragment.java
@@ -6,9 +6,13 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import iview.wsienski.quizapp.R;
 import iview.wsienski.quizapp.di.component.DaggerFragmentCompontent;
@@ -17,6 +21,7 @@ import iview.wsienski.quizapp.network.dao.Quizzes;
 import iview.wsienski.quizapp.ui.activity.MainActivity;
 import iview.wsienski.quizapp.ui.presenter.QuizzesPresenterImpl;
 import iview.wsienski.quizapp.ui.view.QuizzesView;
+import timber.log.Timber;
 
 /**
  * Created by Witold Sienski on 26.07.2016.
@@ -28,6 +33,8 @@ public class QuizzesFragment extends Fragment implements QuizzesView{
     QuizzesPresenterImpl quizzesPresenter;
     FragmentCompontent fragmentCompontent;
     View view;
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
 
     @Nullable
     @Override
@@ -44,16 +51,23 @@ public class QuizzesFragment extends Fragment implements QuizzesView{
 
     @Override
     public void showProgress(boolean show) {
+        Timber.d("showProgress");
+        int visible = show ? View.VISIBLE : View.GONE;
+        progressBar.setVisibility(visible);
     }
 
     @Override
     public void showQuizzes(Quizzes quizzes) {
-
     }
 
     @Override
     public void showError(String message) {
-
+        Timber.d("showError");
+        new MaterialDialog.Builder(getActivity())
+                .title(R.string.dialog_aplogize_title)
+                .content(message)
+                .negativeText(R.string.dialog_button_ok)
+                .show();
     }
 
     @Override
